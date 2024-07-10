6dc445def88146cab73ff46545b23d655f464f2a
michagonzo77 <caleb.g@nylas.com>
2024-07-10 00:00:45 -0700

    fixes

--- src/openshift/openshift-cli.sh
+++ src/openshift/openshift-cli.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Debugging function
 function debug {
     echo "DEBUG: $1"
@@ -9,14 +10,31 @@ function error {
     exit 1
 }
 
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --command) command="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+# Check for required argument
+if [ -z "${command}" ]; then
+    echo "Usage: $0 --command <command>"
+    exit 1
+fi
+
 # Login to OpenShift
 debug "Logging in to OpenShift"
 oc login $OPENSHIFT_API_URL --username=$OPENSHIFT_USERNAME --password=$OPENSHIFT_PASSWORD --insecure-skip-tls-verify=true
 
+if [ $? -ne 0 ]; then
+    error "Failed to log in to OpenShift"
+fi
+
 debug "Successfully logged in to OpenShift"
 
-# Extract and execute the OpenShift CLI command
-command="{{.command}}"
+# Execute the OpenShift CLI command
 debug "Executing OpenShift CLI command: oc $command"
-
 oc $command || error "Failed to execute command: oc $command"
